c9542be4d5e1fc7ba4d9cb9e3398f1d79d0f5fb7
brunobarmac <brunobarmac.0@gmail.com>
2026-03-12 22:28:01 -0300

    continuação

--- studyPro/routes/web.php
+++ studyPro/routes/web.php
@@ -1,18 +1,23 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChatController;
 
-// Rota para mostrar a página inicial do chat
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+// 1. Rota Raiz: Agora ao acessar http://localhost:8000/ ele abre o seu chat
+Route::get('/', function () {
+    return view('chat');
+});
+
+// 2. Rota auxiliar: Caso você ainda queira acessar via http://localhost:8000/chat
 Route::get('/chat', function () {
     return view('chat');
 });
 
-// Rota que recebe a pergunta via POST
+// 3. Rota de Processamento: Onde o formulário envia a pergunta para a IA
 Route::post('/chat/enviar', [ChatController::class, 'perguntar'])->name('chat.enviar');
-
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
